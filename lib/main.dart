73ba1c7759ab7e4877e297f0e82c2cbdcbb5b23b
Gabriele Peck <peck.gabriele@gmail.com>
2024-02-18 16:29:04 -0700

    added AppBar with title and some text on the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle h2 = GoogleFonts.lato().copyWith(fontSize: 18);
+ColorScheme colorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 133, 20, 127));
 
 void main() {
-  runApp(const MaterialApp(home: MyApp()) );
+  runApp(
+    MaterialApp(
+      theme: ThemeData.light(useMaterial3: true)
+          .copyWith(colorScheme: colorScheme),
+      debugShowCheckedModeBanner: false,
+      home: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +24,25 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Facial Recognition Punch In/Out"),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Hello World!"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome to work. Use the camera to clock in.",
+                  style: h2,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
