286ed2121c65a7a989f7c680b3b1d63e09dcffd9
Gabriele Peck <peck.gabriele@gmail.com>
2024-02-18 15:45:27 -0700

    completed a 'Hello World!' proof of concept

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()) );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Facial Recognition Punch In/Out"),),
+      appBar: AppBar(
+        title: const Text("Facial Recognition Punch In/Out"),
+      ),
+      body: const Center(
+        child: Text("Hello World!"),
+      ),
     );
   }
 }
