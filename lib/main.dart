1033d8c541f004e97fa7ad840b4cdbb6f0e2c072
Gabriele Peck <peck.gabriele@gmail.com>
2024-02-25 15:27:38 -0700

    added code to initialize and get a list of cameras

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:facial_reg/screens/home.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// @@riverpod
+// ThemeData appTheme(Ref ref) {
+//   return ThemeData;
+// }
 
 TextStyle h2 = GoogleFonts.lato().copyWith(fontSize: 18);
 ColorScheme colorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 133, 20, 127));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 4, 95, 23));
 
-void main() {
+void main() async {
+  // ensure initialization before accessing cameras
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // get a list of available cameras
+  final cameras = await availableCameras();
+
+  // select a camera from the list
+  final webcam = cameras.first;
+
   runApp(
-    MaterialApp(
-      theme: ThemeData.light(useMaterial3: true)
-          .copyWith(colorScheme: colorScheme),
-      debugShowCheckedModeBanner: false,
-      home: const MyApp(),
+    ProviderScope(
+      child: MaterialApp(
+        theme: ThemeData.dark(useMaterial3: true)
+            .copyWith(colorScheme: colorScheme),
+        debugShowCheckedModeBanner: false,
+        home: const MyApp(),
+      ),
     ),
   );
 }
@@ -21,29 +40,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Facial Recognition Punch In/Out"),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Welcome to work. Use the camera to clock in.",
-                  style: h2,
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+    return const Home();
   }
 }
