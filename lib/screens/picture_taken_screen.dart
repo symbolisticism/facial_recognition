05ff0bd203979763ff32db5b46babadeaf4b4daa
Gabriele Peck <peck.gabriele@gmail.com>
2024-02-25 17:24:01 -0700

    added the PictureTakenScreen to display the picture that was taken

---
+++ lib/screens/picture_taken_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class PictureTakenScreen extends StatelessWidget {
+  const PictureTakenScreen({super.key, required this.imagePath});
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Analyzing picture..."),
+      ),
+      body: Image.file(
+        File(imagePath),
+      ),
+    );
+  }
+}
