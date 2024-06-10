e4c9fd97d486b540ae0d66fbff71f11eb89ea9ba
Gabriele Peck <peck.gabriele@gmail.com>
2024-06-09 23:04:07 -0700

    successfully modeled the api response outcomes in the code. some of it is only modeled at the moment and still needs actual implementation.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,11 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:facial_reg/screens/picture_taken_screen.dart';
+import 'dart:convert';
 import 'package:logger/logger.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 var logger = Logger(printer: PrettyPrinter());
+var db = FirebaseFirestore.instance;
 
 class Home extends StatefulWidget {
   const Home({super.key, required this.camera});
@@ -42,6 +44,89 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    /// Returns a dummy API response with dummy data
+    /// This function assumes that the machine learning model is
+    /// sending a response that looks something like the following:
+    ///
+    ///  ```dart
+    /// {
+    /// 'identified':true,
+    /// 'firstname':'John',
+    /// 'lastname':'Doe'
+    /// }
+    /// ```
+    ///
+    /// ...or...
+    ///
+    /// ```dart
+    /// {
+    /// 'identified':false
+    /// }
+    /// ```
+    Future<String> mlModelResponse(XFile image) {
+      // send the image
+
+      const person = {
+        'identified': true,
+        'employeeid': '123',
+        'firstname': 'Gabriele',
+        'lastname': 'Peck'
+      };
+
+      // return Future<String>.value(jsonEncode(person));
+      return Future.delayed(
+          const Duration(seconds: 1), () => jsonEncode(person));
+    }
+
+    /// Checks whether the first value of the JSON response is true or false
+    /// This makes the assumption that data returned from the machine
+    ///   learning model looks like the following:
+    ///
+    /// ```dart
+    /// {
+    /// 'identified':true,
+    /// 'firstname':'John',
+    /// 'lastname':'Doe'
+    /// }
+    /// ```
+    bool isPersonRecognized(dynamic jsonResponse) {
+      if (jsonResponse['identified'] == true) {
+        return true;
+      }
+
+      return false;
+    }
+
+    /// Returns a dummy response from a Firestore database
+    Future<bool> firestoreDocExists(dynamic jsonResponse) {
+      logger.d(jsonResponse['employeeid']);
+      final usersRef = db.collection('users').doc(jsonResponse['employeeid']);
+      logger.d('Hello Before');
+      return usersRef.get().then((value) => value.exists ? true : false);
+    }
+
+    /// Returns a dummy response relating the success of the post operation
+    /// to the database
+    Future<bool> addUser() {
+      return Future.delayed(const Duration(seconds: 1), () => true);
+    }
+
+    /// Returns the success of clocking the user in or out
+    Future<bool> clockUser() {
+      return Future.delayed(const Duration(seconds: 1), () => true);
+    }
+
+    /// Returns a JSON string converted to JSON, throws an error
+    ///   if the returned object cannot be encoded as JSON
+    dynamic convertToJson(String response) {
+      try {
+        return jsonDecode(response);
+      } catch (e) {
+        logger.e(e);
+        return {'identified': false, 'error': 'JSON conversion'};
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Facial Recognition Punch In/Out"),
@@ -68,11 +153,51 @@ class _HomeState extends State<Home> {
 
             if (!context.mounted) return;
 
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => PictureTakenScreen(imagePath: image.path),
-              ),
-            );
+            var response = await mlModelResponse(image);
+            var jsonResponse = convertToJson(response);
+
+            // check that the user is recognized and that they have a record in
+            //  the database
+
+            // if the person was recognized by the ML model and their record was
+            //  found in the database
+
+            logger.d(isPersonRecognized(jsonResponse)); // true
+            logger.d(await firestoreDocExists(jsonResponse)); // false
+
+            if (isPersonRecognized(jsonResponse) &&
+                await firestoreDocExists(jsonResponse)) {
+              if (!context.mounted) return;
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text('The user was found.'),
+                duration: Duration(seconds: 3),
+              ));
+
+              // if the person was either recognized by the ML model or a record
+              //  was found in the database, but not both
+            } else if (isPersonRecognized(jsonResponse) ||
+                await firestoreDocExists(jsonResponse)) {
+              if (!context.mounted) return;
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text(
+                    'The user is either recognized or recorded, but not both.'),
+                duration: Duration(seconds: 3),
+              ));
+
+              // if neither the person was recognized by the ML model nor a record
+              //  was found in the database for them
+            } else {
+              // show that the user could not be found
+              if (!context.mounted) return;
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text(
+                    'The user could not be identified. Please register as a new user or try again.'),
+                duration: Duration(seconds: 3),
+              ));
+            }
           } catch (e) {
             logger.e(e);
           }
