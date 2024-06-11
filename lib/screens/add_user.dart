a9626005dab14ae350f2635e92c0f09e88c382f8
Gabriele Peck <peck.gabriele@gmail.com>
2024-06-11 00:06:26 -0700

    added an add user screen for new users to put in their information

---
+++ lib/screens/add_user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddUser extends StatelessWidget {
+  const AddUser({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('User Registration'),),
+    );
+  }
+}
