903e1e54493dc5aee93caed9d94d1c4c359c83b1
Gabriele Peck <peck.gabriele@gmail.com>
2024-06-18 13:18:14 -0700

    removed MyApp since it was just a useless middle man, passed camera through the sign in page to the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:camera/camera.dart';
+import 'package:facial_reg/screens/sign_in.dart';
 import 'package:flutter/material.dart';
-import 'package:facial_reg/screens/home.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 // firebase
 import 'package:firebase_core/firebase_core.dart';
@@ -31,19 +31,8 @@ void main() async {
         ).copyWith(
           colorScheme: colorScheme),
         debugShowCheckedModeBanner: false,
-        home: MyApp(camera: webcam),
+        home: SignIn(camera: webcam),
       ),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key, required this.camera});
-
-  final CameraDescription camera;
-
-  @override
-  Widget build(BuildContext context) {
-    return Home(camera: camera);
-  }
 }
