f44ebb47b33475e148c07b29ccce1519349c0714
Gabriele Peck <peck.gabriele@gmail.com>
2024-06-19 16:43:50 -0700

    removed providerscope, set home to WidgetTree rather than SignIn

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:camera/camera.dart';
-import 'package:facial_reg/screens/sign_in.dart';
+import 'package:facial_reg/widgets/widget_tree.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-// firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 ColorScheme colorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 138, 77, 42), brightness: Brightness.dark);
 
-void main() async {
+Future<void> main() async {
   // ensure initialization before accessing cameras
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -24,15 +22,13 @@ void main() async {
   final webcam = cameras.first;
 
   runApp(
-    ProviderScope(
-      child: MaterialApp(
+      MaterialApp(
         theme: ThemeData(
           useMaterial3: true,
         ).copyWith(
           colorScheme: colorScheme),
         debugShowCheckedModeBanner: false,
-        home: SignIn(camera: webcam),
+        home: WidgetTree(camera: webcam),
       ),
-    ),
   );
 }
