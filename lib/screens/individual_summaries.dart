9f5c4ec84becf17f83ba5dacd75cdeab16d04068
Gabriele Peck <peck.gabriele@gmail.com>
2024-07-12 04:31:54 -0700

    added a stub screen for aggregated reports of individual employees' hours

---
+++ lib/screens/individual_summaries.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IndividualSummaries extends StatelessWidget {
+  const IndividualSummaries({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Individual Summaries'),
+      ),
+    );
+  }
+}
