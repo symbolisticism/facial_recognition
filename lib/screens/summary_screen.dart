9f11c99d337eb1945739992f359f87b04e0d08cb
Gabriele Peck <peck.gabriele@gmail.com>
2024-07-12 04:32:30 -0700

    added a summary screen accessible by the admin that gives access to the individual summaries as well as the most recent clocks

---
+++ lib/screens/summary_screen.dart
@@ -0,0 +1,56 @@
+import 'package:facial_reg/screens/recent_clocks.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:facial_reg/screens/individual_summaries.dart';
+
+var auth = FirebaseAuth.instance;
+
+class SummaryScreen extends StatefulWidget {
+  const SummaryScreen({super.key});
+
+  @override
+  State<SummaryScreen> createState() => _SummaryScreenState();
+}
+
+class _SummaryScreenState extends State<SummaryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: true,
+      onPopInvoked: (didPop) {
+        auth.signOut();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Summaries'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const RecentClocks(),
+                  ),
+                );
+              },
+              child: const Text('Recent Clock Activity'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const IndividualSummaries(),
+                  ),
+                );
+              },
+              child: const Text('Individual Work Summary'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
