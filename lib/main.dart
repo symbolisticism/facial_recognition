5c1f610c69d2747ef3cbb39ec4ca3788be82ff34
Gabriele Peck <peck.gabriele@gmail.com>
2024-07-13 21:29:55 -0700

    changed the code back to only look for one camera

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:facial_reg/widgets/widget_tree.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:logger/logger.dart';
 
-ColorScheme colorScheme =
-    ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 138, 77, 42), brightness: Brightness.dark);
+var logger = Logger(printer: PrettyPrinter());
+
+ColorScheme colorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(0, 138, 77, 42),
+    brightness: Brightness.dark);
 
 Future<void> main() async {
   // ensure initialization before accessing cameras
@@ -16,19 +20,24 @@ Future<void> main() async {
   );
 
   // get a list of available cameras
-  final cameras = await availableCameras();
+  final camerasList = await availableCameras();
+
+  CameraDescription camera = camerasList.first;
 
   // select a camera from the list
-  final webcam = cameras.first;
+  // if (camerasList.first.lensDirection == CameraLensDirection.front) {
+  //   camera = camerasList.first;
+  // } else {
+  //   camera = camerasList[1];
+  // }
 
   runApp(
-      MaterialApp(
-        theme: ThemeData(
-          useMaterial3: true,
-        ).copyWith(
-          colorScheme: colorScheme),
-        debugShowCheckedModeBanner: false,
-        home: WidgetTree(camera: webcam),
-      ),
+    MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ).copyWith(colorScheme: colorScheme),
+      debugShowCheckedModeBanner: false,
+      home: WidgetTree(camera: camera),
+    ),
   );
 }
